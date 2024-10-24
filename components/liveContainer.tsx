312db7ecb3156251a697071a0bb3bd1d68a276aa
김민석 <minseok@gimminseog-ui-MacBookPro.local>
2024-10-24 18:24:40 +0900

    deploy

--- components/liveContainer.tsx
+++ components/liveContainer.tsx
@@ -9,17 +9,13 @@ import { IoRainyOutline } from 'react-icons/io5';
 const LiveContainer = () => {
     const [todayLive, setTodayLive] = useState(false);
     const [realTime, setRealTime] = useState<string>('');
-    useEffect(()=>{
-        setInterval(() => {
-            setRealTime(moment().format('LTS'));
-        }, 1000);
-    },[])
+    const [liveState, setLiveState] = useState(false);
     return (
-        <div className={`absolute left-[20px] top-[20px] w-[300px] p-[20px] bg-[#f1f2f6] shadow-xl rounded-[14px] cursor-pointer transition-all hover:scale-[1.02] ${realTime === '' && '!hidden'}`} onClick={()=> setTodayLive(!todayLive)}>
-            <h4 className='text-[#FFB7B7]'>오늘은 {moment().format('YYYY년 MM월 DD일')}</h4>
-            <h4 className='text-[#FFB7B7]'>지금은 {realTime}</h4>
+        <div className={`absolute left-[20px] top-[20px] w-[300px] p-[20px] bg-[#f1f2f6] shadow-xl rounded-[14px] cursor-pointer transition-all hover:scale-[1.02]`} onClick={()=> setTodayLive(!todayLive)}>
+            {/* <h4 className='text-[#FFB7B7]'>오늘은 {moment().format('YYYY년 MM월 DD일')}</h4>
+            <h4 className='text-[#FFB7B7]'>지금은 {realTime}</h4> */}
             <div className="font-semibold text-base flex justify-start items-center mt-[10px]">
-                {
+                {/* {
                     todayLive ?
                     (
                         <MdSunny className='text-[34px] text-[#F79F1F]'/>
@@ -28,8 +24,9 @@ const LiveContainer = () => {
                     (
                         <IoRainyOutline className='text-[34px] text-[#747d8c]'/>
                     )
-                }
-                <h4 className='text-[24px] pl-[6px]'>{todayLive ? '오후 9시에 만나요!' : '오늘은 휴방입니다!'}</h4>
+                } */}
+                <h4 className='text-[24px]'>{todayLive ? '오후 9시에 만나요!' : '오늘은 휴방입니다!'}</h4>
+                <span className='absolute right-0 text-blue-500' onClick={()=> window.open('https://ch.sooplive.co.kr/lumiynus2/post/137781715')}>더보기</span>
             </div>
                 
             <div className="w-full flex justify-start items-center mt-3">
