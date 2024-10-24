08212a88db518b6546ed8be75ad8c971b0d375ab
김민석 <minseok@gimminseog-ui-MacBookPro.local>
2024-10-24 17:09:35 +0900

    deploy

--- components/title.tsx
+++ components/title.tsx
@@ -1,17 +1,8 @@
-import styled from "styled-components";
-
-const Container = styled.div`
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-`
-
 const TitleComponent = () => {
     return (
-        <Container className="w-full flex justify-center items-center">
+        <div className="w-full flex justify-center items-center">
             <h1 className="w-full text-center text-6xl">희영왕국에 오신것을 환영합니다!</h1>
-        </Container>
+        </div>
     )
 }
 
